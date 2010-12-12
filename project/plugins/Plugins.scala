25cf371c86bbfd08a507f46690d25c1ebf3b2560
Konstantin_Grigoriev <Konstantin.V.Grigoriev@gmail.com>
2010-12-12 18:45:04 +0300

    Code coverage plugin has been added.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -4,5 +4,7 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
     val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
     val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.1-SNAPSHOT"
     val resfilt = "com.bumnetworks.resfilt" % "resfilt" % "0.0.1"
+    val scctRepo = "scct-repo" at "http://mtkopone.github.com/scct/maven-repo/"
+    lazy val scctPlugin = "reaktor" % "sbt-scct-for-2.8" % "0.1-SNAPSHOT"
 }
 
