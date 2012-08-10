0afb2be4debbed08003fe4bead49159b2a4191b1
Konstantin_Grigoriev <Konstantin.V.Grigoriev@gmail.com>
2012-08-10 23:46:27 +0400

    Serializers: storage type has been fixed for v1 API.

--- src/test/scala/adjutrix/cliens/model/serializer/json/StorageTypeSerializerSpec.scala
+++ src/test/scala/adjutrix/cliens/model/serializer/json/StorageTypeSerializerSpec.scala
@@ -7,15 +7,18 @@ class StorageTypeSerializerSpec extends SerializerSpec {
 
   "deserialize" should {
     "return correct result" in {
-      StorageTypeSerializer.deserialize( """{"name" : "Bill", "id" : 1}""") must beEqualTo(StorageType("Bill", Some(1)))
+      StorageTypeSerializer.deserialize(
+        """{"name" : "Bill",
+            "id" :"1",
+            "resource_uri": "/api/v1/storagetype/1/"}""") must beEqualTo(StorageType("Bill", Some(1), Some("/api/v1/storagetype/1/")))
     }
   }
   "serialize" should {
     "not fail if id is Some" in {
-      StorageTypeSerializer.serialize(new StorageType("Bill", Some(1))) must equalTo( """{"name":"Bill","id":1}""")
+      StorageTypeSerializer.serialize(StorageType("Bill", Some(1), Some("/api/v1/storagetype/1/"))) must equalTo( """{"name":"Bill","id":"1","resource_uri":"/api/v1/storagetype/1/"}""")
     }
     "not fail if id is None" in {
-      StorageTypeSerializer.serialize(new StorageType("Bill")) must equalTo( """{"name":"Bill"}""")
+      StorageTypeSerializer.serialize(StorageType("Bill")) must equalTo( """{"name":"Bill"}""")
     }
   }
 }
