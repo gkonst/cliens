d0dd1bae1075df0cd5e4a73c416dc01955dbfed2
Konstantin_Grigoriev <Konstantin.V.Grigoriev@gmail.com>
2012-09-23 22:24:31 +0400

    Serializers: storage serializer fixed.

--- src/main/scala/adjutrix/cliens/model/serializer/json/StorageSerializer.scala
+++ src/main/scala/adjutrix/cliens/model/serializer/json/StorageSerializer.scala
@@ -1,5 +1,15 @@
 package adjutrix.cliens.model.serializer.json
 
-import adjutrix.cliens.model.Storage
+import adjutrix.cliens.model._
 
-object StorageSerializer extends JSONSerializer[Storage]
+object StorageSerializer extends JSONSerializer[Storage] {
+  override protected def transformToEntity = super.transformToEntity orElse {
+    case "storageType" => ("type", classOf[Related[_]], {
+      case v => v
+    })
+  }
+
+  override protected def transformToJson = super.transformToJson orElse {
+    case ("storageType", v) => ("type", v)
+  }
+}
