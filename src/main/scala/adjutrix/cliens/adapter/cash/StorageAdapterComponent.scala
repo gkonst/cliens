5eac61c55014ae492154ce1d9143021719b85116
Konstantin_Grigoriev <Konstantin.V.Grigoriev@gmail.com>
2012-09-30 23:45:04 +0400

    Adapter: ad-hoc complex adapter implementation for Storage.

--- src/main/scala/adjutrix/cliens/adapter/cash/StorageAdapterComponent.scala
+++ src/main/scala/adjutrix/cliens/adapter/cash/StorageAdapterComponent.scala
@@ -1,22 +1,38 @@
 package adjutrix.cliens.adapter.cash
 
 import adjutrix.cliens.adapter.{ReaderAdapter, ReaderAdapterComponent}
-import adjutrix.cliens.model.Storage
+import adjutrix.cliens.model._
 import adjutrix.cliens.conf.{Configurable, PropertiesConfigurable}
 import adjutrix.cliens.adapter.protocol.{JSONProtocol, Protocol}
 import adjutrix.cliens.model.serializer.json.cash.StorageSerializer
 
 trait StorageAdapterComponent extends ReaderAdapterComponent[Storage] {
 
-  lazy val adapter: StorageAdapter =
-    new StorageAdapter with PropertiesConfigurable with JSONProtocol[Storage] {
-      lazy val serializer = StorageSerializer
-    }
+  lazy val adapter: StorageAdapter = new StorageComplexAdapter with PropertiesConfigurable with JSONProtocol[Storage] {
+    lazy val serializer = StorageSerializer
+  }
 
   trait StorageAdapter extends ReaderAdapter[Storage] {
     self: Configurable with Protocol[Storage] =>
 
     val baseUrl = "storage"
+  }
+
+  trait StorageComplexAdapter extends StorageAdapter {
+    self: Configurable with Protocol[Storage] =>
+
+    lazy val currencyTypeAdapter = new CurrencyTypeAdapterComponent {}.adapter
+
+    override def findAll() = {
+      super.findAll() fold(l => Left(l), r => Right(r map {
+        _.map(fullFill)
+      }))
+    }
+
+    def fullFill(storage: Storage): Storage = {
+      storage.copy(currencyType =
+        currencyTypeAdapter.findById(uriToId(storage.currencyType.resourceURI)).right.get.get)
+    }
   }
 
 }
