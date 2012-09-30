610b4ca599aaf98d135128ed46f5cc94416c9863
Konstantin_Grigoriev <Konstantin.V.Grigoriev@gmail.com>
2012-09-30 15:45:25 +0400

    Obsolete serializers removed.

--- src/main/scala/adjutrix/cliens/model/serializer/Serializers.scala
+++
@@ -1,9 +0,0 @@
-package adjutrix.cliens.model.serializer
-
-import adjutrix.cliens.model._
-
-trait Serializers {
-  def storageSerializer: Serializer[Storage]
-
-  def categorySerializer: Serializer[Category]
-}
